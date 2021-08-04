17c65a02857b02ecf61975af4cc83bdbde55681f
manthan1610 <47921976+manthan1610@users.noreply.github.com>
2021-08-04 23:00:57 +0530

    Add files via upload

---
+++ client_sta_imp/client_sta_imp.ino
@@ -0,0 +1,78 @@
+#include <ESP8266WiFi.h>
+#include<Servo.h>
+Servo myservo1;
+Servo myservo2;
+Servo myservo3;
+Servo myservo4;
+Servo myservo5;
+const char* ssid = "Pomiary";
+const char* pass = "pomiar_mocy";
+const int Port = 80;
+IPAddress host(192,168,4,1);
+WiFiClient client;                          
+int z=0;
+String req;
+int pos,pos1;
+void setup() 
+{
+ myservo1.attach(0) ;myservo2.attach(2) ; myservo3.attach(4) ; myservo4.attach(5) ; myservo5.attach(12) ; 
+  delay(10); 
+  Serial.begin(115200);
+  Serial.print("Connecting to: ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);                      
+  WiFi.begin(ssid, pass);                    
+  while (WiFi.status() != WL_CONNECTED) {    
+    delay(5); 
+ //   Serial.print(".");
+  }
+  
+  Serial.println("WiFi connected"); 
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());             
+}
+
+void loop() {
+  if(WiFi.status() != WL_CONNECTED){
+     Serial.println("reconnecting");
+    WiFi.begin(ssid, pass);}
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(50);
+      //Serial.print(".");
+      }
+    conn:  
+    client.connect(host,Port);
+    client.flush();
+while(!client.connected()){
+    client.flush();  
+    goto conn;
+ //   Serial.println("\nCoonection failed\n");
+  }
+client.flush();
+if(client.connected()){
+  String k=client.readStringUntil('\r');
+  int pos=k.toInt();
+  if(pos>=0&&pos<=180)
+  {
+    Serial.print("Servo 1 angle is ");Serial.println(pos);myservo1.write(pos);
+    pos1=map(pos,0,180,180,0);myservo2.write(pos1);
+  }
+  else if(pos>=181&&pos<=361)
+  {
+    Serial.print("Servo 2 angle is ");pos=map(pos,181,361,0,180);Serial.println(pos);myservo3.write(pos);
+  }
+  else if(pos>=542&&pos<=812)
+  {
+    Serial.print("Servo 3 angle is ");pos=map(pos,542,812,0,270);Serial.println(pos);myservo4.write(pos);
+    }
+    else if(pos>=813&&pos<=1083)
+  {
+    Serial.print("Servo 4 angle is ");pos=map(pos,813,1083,0,270);Serial.println(pos);myservo5.write(pos);
+    }
+    
+  client.flush();
+  delay(10);
+  z++;}
+  
+    
+}
---
+++ spiserver_/spiserver_.ino
@@ -0,0 +1,95 @@
+#define SELPIN 15 //Selection Pin 
+#define DATAOUT 13//MOSI 
+#define DATAIN  12//MISO 
+#define SPICLOCK  14//Clock 
+#include <ESP8266WiFi.h>
+const char *ssid = "Pomiary";
+const char *pass = "pomiar_mocy";
+const int httpPort = 80;
+int x;int i=0;
+int  token= 1;                    //number of measuring device to ask
+WiFiServer server(httpPort);
+String req="";
+int z=0;
+int readvalue1,readvalue2; 
+int readvalue[4];
+
+void setup(){ 
+ //set pin modes 
+ delay(100);
+  Serial.begin(115200);
+  Serial.println("Configuring AP");
+  WiFi.mode(WIFI_AP);
+  boolean result = WiFi.softAP(ssid,pass);
+  if(result == true) Serial.println("Ready");
+  else Serial.println("Failed!");
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  server.begin();
+  Serial.println("HTTP server started");
+
+  delay(1000);
+ pinMode(SELPIN, OUTPUT); 
+ pinMode(DATAOUT, OUTPUT); 
+ pinMode(DATAIN, INPUT); 
+ pinMode(SPICLOCK, OUTPUT); 
+ //disable device to start with 
+ digitalWrite(SELPIN,HIGH); 
+ digitalWrite(DATAOUT,LOW); 
+ digitalWrite(SPICLOCK,LOW); 
+} 
+
+int read_adc(int channel){
+  int adcvalue = 0;
+  byte commandbits = B11000000; //command bits - start, mode, chn (3), dont care (3)
+
+  //allow channel selection
+  commandbits|=((channel-1)<<3);
+
+  digitalWrite(SELPIN,LOW); //Select adc
+  // setup bits to be written
+  for (int i=7; i>=3; i--){
+    digitalWrite(DATAOUT,commandbits&1<<i);
+    //cycle clock
+    digitalWrite(SPICLOCK,HIGH);
+    digitalWrite(SPICLOCK,LOW);    
+  }
+
+  digitalWrite(SPICLOCK,HIGH);    //ignores 2 null bits
+  digitalWrite(SPICLOCK,LOW);
+  digitalWrite(SPICLOCK,HIGH);  
+  digitalWrite(SPICLOCK,LOW);
+
+  //read bits from adc
+  for (int i=11; i>=0; i--){
+    adcvalue+=digitalRead(DATAIN)<<i;
+    //cycle clock
+    digitalWrite(SPICLOCK,HIGH);
+    digitalWrite(SPICLOCK,LOW);
+  }
+  digitalWrite(SELPIN, HIGH); //turn off device
+  return adcvalue;
+}
+
+
+void loop() { 
+WiFiClient client = server.available();
+ if  (client.connected())
+ { 
+ client.flush();
+  readvalue[i] = read_adc(1+i); 
+if(i==0){if(readvalue[i]>=448&&readvalue[i]<=3812){readvalue[i]=map(readvalue[i],3812,448,0,180);}}
+else if(i==1){if(readvalue[i]>=0&&readvalue[i]<=2600){readvalue[i]=map(readvalue[i],0,2600,181,361);}}
+else if(i==2) {readvalue[i]=map(readvalue[i],7,4095,542,812);}
+else if(i==3){readvalue[i]=map(readvalue[i],7,4095,813,1083);}
+
+ client.println(readvalue[i]);
+Serial.println(readvalue[i]); 
+if(i==3){i=0;}
+else {i++;}
+delay(40);
+} 
+
+}
+ 
